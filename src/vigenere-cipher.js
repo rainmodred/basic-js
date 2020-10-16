66aae2fc06875c5aca3c8b81eac9364e8cd83445
phoenix <albphoenix@gmail.com>
2020-10-16 11:28:43 +0300

    feat: add vigenere cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,109 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.letters = [
+      'a',
+      'b',
+      'c',
+      'd',
+      'e',
+      'f',
+      'g',
+      'h',
+      'i',
+      'j',
+      'k',
+      'l',
+      'm',
+      'n',
+      'o',
+      'p',
+      'q',
+      'r',
+      's',
+      't',
+      'u',
+      'v',
+      'w',
+      'x',
+      'y',
+      'z',
+    ];
+    this.direct = direct;
+  }
+
+  _generateKeyword(message, key) {
+    let length = message.split(' ').join('').length;
+    let keyword = '';
+
+    let i = 0;
+    while (keyword.length < length) {
+      keyword += key[i];
+      i++;
+      if (i === key.length) {
+        i = 0;
+      }
+    }
+    return keyword;
+  }
+
+  _doCrypt(message, key, isEncrypt) {
+    let keyword = this._generateKeyword(message, key).toLowerCase();
+    let index = 0;
+    let cryptedMessage = message
+      .split(' ')
+      .map((word) =>
+        word
+          .split('')
+          .map((letter) => {
+            if (
+              letter.charCodeAt(0) < 65 ||
+              (letter.charCodeAt(0) > 90 && letter.charCodeAt(0) < 97) ||
+              letter.charCodeAt(0) > 122
+            ) {
+              return letter;
+            }
+
+            let messageLetterIndex = this.letters.indexOf(letter.toLowerCase());
+            let keywordLetterIndex = this.letters.indexOf(keyword[index]);
+            index++;
+
+            if (isEncrypt) {
+              return this.letters[
+                (messageLetterIndex + keywordLetterIndex) % this.letters.length
+              ].toUpperCase();
+            }
+            return this.letters[
+              (messageLetterIndex - keywordLetterIndex + this.letters.length) %
+                this.letters.length
+            ].toUpperCase();
+          })
+          .join('')
+      )
+      .join(' ');
+
+    if (!this.direct) {
+      return cryptedMessage.split('').reverse().join('');
+    }
+
+    return cryptedMessage;
+  }
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('');
+    }
+
+    return this._doCrypt(message, key, true);
+  }
+  
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error('');
+    }
+
+    return this._doCrypt(encryptedMessage, key, false);
   }
 }
 
